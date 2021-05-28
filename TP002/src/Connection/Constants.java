40ae51cd6d9e50cfae603d8e07aa6a812346099f
Diệp Gia Hữu <83325530+huudg@users.noreply.github.com>
2021-05-28 09:23:43 +0700

    Update Constants.java

--- TP002/src/Connection/Constants.java
+++ TP002/src/Connection/Constants.java
@@ -2,6 +2,6 @@ package Connection;
 
 public class Constants {
 	public static final String MY_EMAIL = "huudg.tester@gmail.com";
-	public static final String MY_PASSWORD ="Maysetroi022021";
+	public static final String MY_PASSWORD ="";
 	public static final String FRIEND_EMAIL = "diepgiahuu99@gmail.com";  
 }
